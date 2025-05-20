45d9d9f77afb767bf7865289f55c4e0f90769f5a
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-19 23:48:17 -0300

    Update agrupar_dados.py

--- estoque/agrupar_dados.py
+++ estoque/agrupar_dados.py
@@ -2,4 +2,5 @@ def produtos_baixo_estoque(df_estoque):
     produtos_estoque_baixo=df_estoque.loc[
         ((df_estoque["Unidade"]=="UN") & (df_estoque["Quantidade"]<10)) |
         ((df_estoque["Unidade"]=="KG") & (df_estoque["Quantidade"]<1))]
+    produtos_estoque_baixo=produtos_estoque_baixo.sort_values(by="Quantidade")
     return produtos_estoque_baixo
