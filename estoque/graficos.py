c536224007f5267e5625051da4aae9be05580518
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-20 20:42:31 -0300

    Update graficos.py

--- estoque/graficos.py
+++ estoque/graficos.py
@@ -5,7 +5,6 @@ def grafico_valor_por_categoria(df_estoque_por_categoria):
     fig=px.pie(df_estoque_por_categoria,
                names="Categoria",
                values="Custo Total",
-               color="Categoria",
                title="Valor em Estoque por Categoria")
     fig.update_layout(xaxis_title="Categoria",
                       yaxis_title="Custo Total",
