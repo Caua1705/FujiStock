bc3ee2e1ba8b867c22c3704278830a54224f8b36
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-20 20:36:15 -0300

    Update graficos.py

--- estoque/graficos.py
+++ estoque/graficos.py
@@ -2,7 +2,13 @@ import streamlit as st
 import plotly.express as px
 
 def grafico_valor_por_categoria(df_estoque_por_categoria):
-    fig=px.pie(df_estoque_por_categoria,names="Categoria",values="Custo Total",title="Valor em Estoque por Categoria")
+    fig=px.pie(df_estoque_por_categoria,
+               names="Categoria",
+               values="Custo Total",
+               title="Valor em Estoque por Categoria")
+    fig.update_layout(xaxis_title="Categoria",
+                      yaxis_title="Custo Total",
+                      showlegend=True)
     st.plotly_chart(fig)
     st.write(df_estoque_por_categoria)
 
