7441546bc0e5fec7ebc76e2b62808ec799084282
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-20 20:39:06 -0300

    Update graficos.py

--- estoque/graficos.py
+++ estoque/graficos.py
@@ -5,10 +5,12 @@ def grafico_valor_por_categoria(df_estoque_por_categoria):
     fig=px.pie(df_estoque_por_categoria,
                names="Categoria",
                values="Custo Total",
+               color="Categoria",
                title="Valor em Estoque por Categoria")
     fig.update_layout(xaxis_title="Categoria",
                       yaxis_title="Custo Total",
                       showlegend=True)
+    fig.update_traces(textinfo="percent+label")       
     st.plotly_chart(fig)
     st.write(df_estoque_por_categoria)
 
