c3ad6aa20b6f8bc9a4837e7b392953552c37cf98
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-20 21:26:27 -0300

    Update graficos.py

--- estoque/graficos.py
+++ estoque/graficos.py
@@ -10,15 +10,10 @@ def grafico_valor_por_categoria(df_estoque_por_categoria):
                text="Custo Total",
                color="Categoria"
                )
-    fig.update_traces(texttemplate="R$ %{x:,.2f}", textposition="outside")
     fig.update_layout(
         xaxis_title="Custo Total (R$)",
         yaxis_title="Categoria",
-        xaxis_tickformat=",.2f",
-        yaxis=dict(tickmode="linear"),
         showlegend=False,
-        margin=dict(l=100, r=40, t=80, b=40),
-        height=400
     )
     st.plotly_chart(fig,use_container_width=True)
     st.write(df_estoque_por_categoria)
