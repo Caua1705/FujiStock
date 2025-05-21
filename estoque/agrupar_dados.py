78764cd4db3882f924c3a8473305f0c8bf1464e9
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-20 21:20:14 -0300

    Update agrupar_dados.py

--- estoque/agrupar_dados.py
+++ estoque/agrupar_dados.py
@@ -3,7 +3,7 @@ def produtos_baixo_estoque(df_estoque):
     produtos_estoque_baixo=df_estoque.loc[
         ((df_estoque["Unidade"]=="UN") & (df_estoque["Quantidade"]<10)) |
         ((df_estoque["Unidade"]=="KG") & (df_estoque["Quantidade"]<1))]
-    produtos_estoque_baixo=produtos_estoque_baixo.sort_values(by="Quantidade",ascending=True)
+    produtos_estoque_baixo=produtos_estoque_baixo.sort_values(by="Quantidade",)
     return produtos_estoque_baixo
 
 def custo_total_por_categoria(df_estoque):
