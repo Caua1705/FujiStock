d100de9e43cbaa2c62336751f1b3c047cc3b6e24
Caua de Carvalho <cauaccarvalho10@gmail.com>
2025-05-20 21:47:50 -0300

    Update metricas.py

--- estoque/metricas.py
+++ estoque/metricas.py
@@ -4,10 +4,10 @@ def criar_metricas(df_catalogo_produtos,df_estoque,produtos_estoque_baixo):
     col1,col2,col3,col4=st.columns(4)
     with col1:
         valor_total_estoque=df_estoque["Custo Total"].sum()
-        st.metric("Valor Total do Estoque",f"R$ {valor_total_estoque:.2f}")
+        st.metric("Valor Total do Estoque",f"R$ {valor_total_estoque:,.2f}")
     with col2:
         st.metric("Produtos Cadastrados",f"{len(df_catalogo_produtos["Nome"])} itens")
     with col3:
-        st.metric("Produtos em Estoque",f"{len(df_estoque["Nome"])} itens")
+        st.metric("Produtos em Estoque",f"{len(df_estoque["Nome"]):03d} itens")
     with col4:
-        st.metric("Produtos com Estoque Baixo",f"{len(produtos_estoque_baixo)} itens")
+        st.metric("Produtos com Estoque Baixo",f"{len(produtos_estoque_baixo):03d} itens")
